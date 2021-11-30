9a55067636c3487c0889b30cb4a49324bf9a526f
Melody Lashgari <melodylashgari@hotmail.com>
2021-11-30 22:27:07 +0100

    last commit

--- main.js
+++ main.js
@@ -25,9 +25,11 @@ function getInput() {
   //Lägg till values till expenses eller income
   if (inputData.type === "true") {
     income.push(inputData);
+    balance.push(income)
     renderTable("income-table", ["Beskrivning", "Summa"], income);
   } else {
     expenses.push(inputData);
+    balance.push(expenses)
     renderTable("expenses-table", ["Beskrivning", "Summa"], expenses);
   }
 }
@@ -64,85 +66,22 @@ function renderTable(tableId, tableHeaderArray, data) {
   let summaE = 0;
   expenses.forEach((singleExpense) => {
     summaE += Number(singleExpense.ammount);
+
     document.querySelector("#expense").textContent = "-" + summaE + "kr";
   });
 //Uppdatera värden i balance
   let summaB = 0;
-  balance.forEach((singleBalance) => {
-    summaB += Number(singleBalance.ammount);
-    document.querySelector("#balance").textContent = summa - summaE + "kr";
+  balance.forEach((singleIncome, singleExpense) => {
+    summaB += Number(singleIncome.ammount, singleExpense.ammount);
+    document.querySelector("#balance").textContent = (summa - summaE) + "kr";
   });
   console.log(balance);
+
 
   //Tar bort alla html child elements från parent element
   function removeAllChildNodes(parent) {
     while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
+      parent.removeChild(parent.firstChild); 
     }
   }
-
-
-//Local Storage
-const localStorageTransactions = JSON.parse(localStorage.getItem("transactions"));
-
-
-//Input elements
-function getInput(){
-    let elements = {
-        type: document.getElementById('input-type'),
-        description: document.getElementById('input-description'),
-        ammount: document.getElementById('input-value'),
-    }
-
-    //Definera data object
-    let inputData = {
-        type: elements.type.value,
-        description: elements.description.value,
-        ammount: elements.ammount.value,
-    }
-
-    //Reset input values
-    elements.type.value = "choose";
-    elements.description.value = null;
-    elements.ammount.value = null;
-
-    //Lägg till values till expenses eller income
-    if(inputData.type === "true"){
-        income.push(inputData);
-        renderTable('income-table', ['Beskrivning', 'Summa'], income);
-    }else{
-        expenses.push(inputData);
-        renderTable('expenses-table', ['Beskrivning', 'Summa'], expenses);
-    }
-}
-// Skapa inkomst och utgift listor
-
-function renderTable(tableId, tableHeaderArray, data){
-    let table = document.getElementById(tableId);
-    removeAllChildNodes(table);
-     let tableHeadRow = document.createElement('tr');
-    tableHeaderArray.forEach((colName)=>{
-        let col = document.createElement('th');
-        col.innerText = colName;
-        tableHeadRow.appendChild(col);
-    });
-    table.appendChild(tableHeadRow);
-    data.forEach((colData)=>{
-        let dataRow = document.createElement('tr');
-        let col1 = document.createElement('td');
-        col1.innerText = colData.description;
-        let col2 = document.createElement('td');
-        col2.innerText = colData.ammount + 'kr';
-        dataRow.appendChild(col1);
-        dataRow.appendChild(col2);
-        table.appendChild(dataRow);
-    });
-
-}
-//Tar bort alla html child elements från parent element
-function removeAllChildNodes(parent) {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
-}
 }
